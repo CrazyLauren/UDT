a80d36c70ab35ce16ca8ebd5ba7cb2d2bec99d69
Sergey <sergey0311@gmail.com>
2019-11-17 02:40:45 +0300

    Update CI

--- SHARE/src/random_value.cpp
+++ SHARE/src/random_value.cpp
@@ -18,6 +18,13 @@
 #include <SHARE/random_value.h>
 #include <SHARE/get_cpuid.h>
 
+#if defined(_MSC_VER)
+/* Microsoft C/C++-compatible compiler */
+     #include <intrin.h>
+#elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
+/* GCC-compatible compiler, targeting x86/x86-64 */
+#   include <x86intrin.h>
+#endif
 namespace NSHARE
 {
 namespace
