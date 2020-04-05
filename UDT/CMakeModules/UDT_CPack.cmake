cd45e9569cce782e6a21a97f084d5fc96cf079f2
Sergey <sergey0311@gmail.com>
2020-04-06 00:03:35 +0300

    Fix NSIS package

--- UDT/CMakeModules/UDT_CPack.cmake
+++ UDT/CMakeModules/UDT_CPack.cmake
@@ -6,10 +6,12 @@ if(CPACK_GENERATOR STREQUAL "NSIS")
          ; set variable for local machine
          WriteRegExpandStr \\\${env_hklm} UDT_CUSTOMER_CONFIG_PATH \\\$INSTDIR\\\\${CMAKE_INSTALL_BINDIR}\\\\default_customer_config.json
          WriteRegExpandStr \\\${env_hklm} UDT_CUSTOMER_MODULE_PATH \\\$INSTDIR\\\\${CMAKE_INSTALL_BINDIR}"
+         SendMessage \\\${HWND_BROADCAST} \\\${WM_WININICHANGE} 0 \\\"STR:Environment\\\" /TIMEOUT=5000
          )
 
         list(APPEND CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS
              "DeleteRegValue \\\${env_hklm} UDT_CUSTOMER_CONFIG_PATH
-              DeleteRegValue \\\${env_hklm} UDT_CUSTOMER_MODULE_PATH
+                 DeleteRegValue \\\${env_hklm} UDT_CUSTOMER_MODULE_PATH
+                 SendMessage \\\${HWND_BROADCAST} \\\${WM_WININICHANGE} 0 \\\"STR:Environment\\\" /TIMEOUT=5000
             ")
 endif()
